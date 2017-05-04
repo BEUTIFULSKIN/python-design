558efaf951eb5bc3e3458f7a8c75f77a2c9d7844
yuanrong <m15575824785@163.com>
2017-05-04 14:59:14 +0800

    添加组合模式

--- Composite/yoke_compsite.py
+++ Composite/yoke_compsite.py
@@ -8,6 +8,68 @@
 #  电子邮箱 : ryuan@jmgo.com
 #    日期   : 2017/5/3 16:51
 #
-#     描述  :
+#     描述  : 组合模式 —— 用于目录结构，是部分 + 整体，部分整体都可对外对立调用
 #
-import os
+
+
+class Composite:
+    def __init__(self):
+        self.index = None
+        self.level = 0
+        self.t_next = None
+
+    def add(self, node):  # 添加节点
+        pass
+
+    def remove(self, node):  # 移除节点
+        pass
+
+    def traverse(self):  # 遍历
+        pass
+
+
+class NodeLeaf(Composite):
+    def __init__(self, t_index):
+        Composite.__init__(self)
+        self.index = t_index
+
+    def add(self, node):
+        print "this is leaf node, can not add"
+
+    def traverse(self):
+        print self.index
+
+
+class NodeComposite(Composite):
+    def __init__(self, t_index):
+        Composite.__init__(self)
+        self.index = t_index
+
+    def add(self, node):
+        if not self.t_next:
+            self.t_next = node
+            self.level += 1
+        else:
+            print "already next node, if you continue add will be change next node"
+
+    def traverse(self):
+        if self.t_next:
+            print self.index
+            self.t_next.traverse()
+
+if __name__ == "__main__":
+    leaf_1 = NodeLeaf('001')
+    c_node_1 = NodeComposite('101')
+    c_node_1.add(leaf_1)
+    leaf_2 = NodeLeaf('002')
+    c_node_2 = NodeComposite('102')
+    c_node_2.add(c_node_1)
+    c_node_2.traverse()
+    leaf_2.add(leaf_1)
+    leaf_2.traverse()
+
+
+
+
+
+
